f1f78e0af695efaec78a8c8403bfa76553aa9ed0
xkrejc70 <xkrejc70@vutbr.cz>
2024-12-09 09:12:18 +0100

    Simple phpunit tests

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -6,13 +6,11 @@ class HelloWorldTest extends PHPUnit\Framework\TestCase
 {
  public function testOutput()
  {
-    // Capture the output of hello.php
     ob_start();
     include '../html/hello.php';
     $output = ob_get_clean();
 
-    // Assert that the output is "Hello, Docker!"
-    $this->assertEquals("Hello, Docker!", $output);
+    $this->assertEquals("Hello, World!", $output);
  }
 }
 ?>
