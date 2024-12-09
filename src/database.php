e7e1cf8e461ba5c794c7301e6cc104ef92a97dd0
xkrejc70 <xkrejc70@vutbr.cz>
2024-12-09 10:41:03 +0100

    Remove comments

--- src/database.php
+++ src/database.php
@@ -1,20 +1,13 @@
 <?php
 
-// Read the database connection parameters from environment variables
 $db_host = getenv('DB_HOST');
 $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
 
-// Read the password file path from an environment variable
 $password_file_path = getenv('PASSWORD_FILE_PATH');
-
-// Read the password from the file
 $db_pass = trim(file_get_contents($password_file_path));
-
-// Create a new PDO instance
 $db_handle = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 
-// Create the "messages" table if it doesn't exist
 $db_handle->exec("
  CREATE TABLE IF NOT EXISTS messages (
      id INT AUTO_INCREMENT PRIMARY KEY,
